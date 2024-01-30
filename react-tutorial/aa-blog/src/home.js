56686f2006a3522c2283b6c23e04dd43249e2593
arifoglu <88225121+arifoglu@users.noreply.github.com>
2024-01-30 11:16:08 +0100

    click events finished

---
+++ react-tutorial/aa-blog/src/home.js
@@ -0,0 +1,15 @@
+const Home = () => {
+
+    const handleClick = () =>{
+        
+    }
+
+    return ( 
+        <div className="home">
+            <h2>HomePage</h2>
+            <button onClick= { handleClick }>Click</button>            
+        </div>
+     );
+}
+ 
+export default Home;
